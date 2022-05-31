ef64df0009ebb3fc90bde65df6ebef7deaf4e290
Gartham <gartham@dusttoash.org>
2022-05-31 01:32:22 -0500

    Update bot

--- src/com/gartham/memebot/BotConfiguration.java
+++ src/com/gartham/memebot/BotConfiguration.java
@@ -4,22 +4,30 @@ import org.alixia.javalibrary.parsers.cli.CLIParams;
 
 public class BotConfiguration {
 
-	/*
-	 * Add properties to configure the bot on startup here. These are parsed from
-	 * command line arguments by the bot.
-	 * 
-	 * A couple example arguments are provided in the class. You can delete them if
-	 * you'd like.
-	 */
-
 	public BotConfiguration(CLIParams cmdLineArguments) {
 		token = cmdLineArguments.readString("", "--token");
+		selectedMemesChannel = cmdLineArguments.readString("682566378514939927", "--selected-memes-channel", "-smc");
+		memesChannel = cmdLineArguments.readString("682621944943738900", "--normal-channel", "--memes-channel", "-nc",
+				"-mc");
+		picker = cmdLineArguments.readString("682427503272525862", "--picker", "-p");
 	}
 
-	private final String token;
+	private final String token, selectedMemesChannel, memesChannel, picker;
 
 	public String getToken() {
 		return token;
+	}
+
+	public String getSelectedMemesChannel() {
+		return selectedMemesChannel;
+	}
+
+	public String getMemesChannel() {
+		return memesChannel;
+	}
+
+	public String getPicker() {
+		return picker;
 	}
 
 }
--- src/com/gartham/memebot/BotLauncher.java
+++ src/com/gartham/memebot/BotLauncher.java
@@ -1,11 +1,18 @@
 package com.gartham.memebot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.security.auth.login.LoginException;
 
 import org.alixia.javalibrary.parsers.cli.CLIParams;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class BotLauncher {
@@ -16,7 +23,30 @@ public class BotLauncher {
 		JDA jda = JDABuilder.createDefault(config.getToken(), GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
 				.build();
 
+		Map<String, Integer> score = new HashMap<>();
+
+		jda.addEventListener((EventListener) event -> {
+			if (event instanceof MessageReactionAddEvent) {
+				var mre = (MessageReactionAddEvent) event;
+				if (mre.getChannel().getId().equals(config.getMemesChannel())
+						&& mre.getUserId().equals(config.getPicker()) && mre.getReactionEmote().isEmoji()
+						&& mre.getReactionEmote().getEmoji().equals("\u2B50")) {
+					Message msg = mre.retrieveMessage().complete();
+					if (msg.getAttachments().size() == 1) {
+						Utilities.sendAsCreature(msg.getMember(),
+								new EmbedBuilder().setDescription("Meme by " + msg.getAuthor().getAsMention() + '.')
+										.setImage(msg.getAttachments().get(0).getUrl()),
+								jda.getTextChannelById(config.getSelectedMemesChannel()));
+						inc(score, msg.getAuthor().getId());
+					}
+				}
+			}
+		});
+
 		// TODO Write out whatever code you need your bot to do after it's logged in.
 	}
 
+	private static <T> void inc(Map<? super T, Integer> map, T val) {
+		map.put(val, map.containsKey(val) ? map.get(val) + 1 : 1);
+	}
 }
